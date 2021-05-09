c2b13454098ec153ec7379eebf90eb029c13d70f
Kevin Furjan <kfurjan@gmail.com>
2021-05-09 19:51:46 +0200

    Added test_users.py

--- Backend/tests/test_api/test_errors.py
+++ Backend/tests/test_api/test_errors.py
@@ -1,33 +1,36 @@
-from fastapi.testclient import TestClient
+import pytest
+from httpx import AsyncClient
 from starlette.status import (
     HTTP_401_UNAUTHORIZED,
     HTTP_404_NOT_FOUND,
     HTTP_422_UNPROCESSABLE_ENTITY,
 )
 
+pytestmark = pytest.mark.asyncio
 
-def test_404_error_handler(client: TestClient):
-    response = client.get("/wrong_path")
+
+async def test_404_error_handler(async_client: AsyncClient):
+    response = await async_client.get("/wrong_path")
 
     assert response.status_code == HTTP_404_NOT_FOUND
     assert response.json() == {"errors": ["Not Found"]}
 
 
-def test_401_jwt_authorization_error_handler(client: TestClient):
-    response = client.get("/api/v1/user")
+async def test_401_jwt_authorization_error_handler(async_client: AsyncClient):
+    response = await async_client.get("/api/v1/user")
 
     assert response.status_code == HTTP_401_UNAUTHORIZED
     assert response.json() == {"detail": "Missing Authorization Header"}
 
 
-def test_422_jwt_authorization_error_handler(client: TestClient):
-    response = client.get(
+async def test_422_jwt_authorization_error_handler(async_client: AsyncClient):
+    response = await async_client.get(
         "/api/v1/user", headers={"Authorization": "Bearer wrong_token"}
     )
     assert response.status_code == HTTP_422_UNPROCESSABLE_ENTITY
     assert response.json() == {"detail": "Not enough segments"}
 
-    response = client.get(
+    response = await async_client.get(
         "/api/v1/user", headers={"Authorization": "token wrong_token"}
     )
     assert response.status_code == HTTP_422_UNPROCESSABLE_ENTITY
