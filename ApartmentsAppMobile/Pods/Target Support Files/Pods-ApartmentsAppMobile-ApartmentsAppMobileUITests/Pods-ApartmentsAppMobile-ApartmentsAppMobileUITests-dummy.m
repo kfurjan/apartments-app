0f9b629b8778fdf0a6214cb9ee9af563d33b20e7
Kevin Furjan <kfurjan@gmail.com>
2021-05-24 22:12:11 +0200

    Adding inital project setup. Added dependencies and SwiftLint

---
+++ ApartmentsAppMobile/Pods/Target Support Files/Pods-ApartmentsAppMobile-ApartmentsAppMobileUITests/Pods-ApartmentsAppMobile-ApartmentsAppMobileUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ApartmentsAppMobile_ApartmentsAppMobileUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ApartmentsAppMobile_ApartmentsAppMobileUITests
+@end
