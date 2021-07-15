68518db975e39e40fabd6376f62436d188c77908
Kresimir Vlahov <vlahov.kresimir@gmail.com>
2021-07-15 21:38:09 +0200

    Increase jwt

--- Backend/app/models/common/jwt.py
+++ Backend/app/models/common/jwt.py
@@ -7,4 +7,4 @@ class Settings(BaseModel):
     authjwt_secret_key: str = str(SECRET_KEY)
     authjwt_denylist_enabled: bool = True
     authjwt_denylist_token_checks: set = {"access"}
-    access_expires: int = timedelta(minutes=20)
+    access_expires: int = timedelta(minutes=120)
